3577dbc42f4a5afb461477fd94743e029e5ed3ac
IEsmabe <iesmabe@gbox.adnu.edu.ph>
2025-10-25 15:36:19 +0800

    Routes for reviews and sessions

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
+// Middleware
 app.use(express.json());
 
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/tutorba', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error('MongoDB connection error:', error);
+});
+
+// Routes
+const reviewsRouter = require('./routes/reviews');
+const sessionsRouter = require('./routes/sessions');
+
+app.use('/api/reviews', reviewsRouter);
+app.use('/api/sessions', sessionsRouter);
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('TutorBa API Server');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
