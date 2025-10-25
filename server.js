2c37502871ddb5f9291579bebaeb0418736a5c73
IEsmabe <iesmabe@gbox.adnu.edu.ph>
2025-10-25 17:07:19 +0800

    Rollback to initial commit

--- server.js
+++ server.js
@@ -1,32 +1,13 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
-// Middleware
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/tutorba', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((error) => {
-    console.error('MongoDB connection error:', error);
-});
-
-// Routes
-const reviewsRouter = require('routes/reviews');
-const sessionsRouter = require('routes/sessions');
-
-app.use('/api/reviews', reviewsRouter);
-app.use('/api/sessions', sessionsRouter);
-
 app.get('/', (req, res) => {
-    res.send('TutorBa API Server');
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
