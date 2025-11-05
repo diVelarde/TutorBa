e2d19d4ceef309ef779580b7b42b3aeef1184b44
diVelarde <divelarde@gbox.adnu.edu.ph>
2025-11-06 04:14:04 +0800

    [TRD-003]-Implement pagination or lazy loading for search results

--- services/tutorService.js
+++ services/tutorService.js
@@ -1,12 +1,14 @@
 import Tutor from "../models/Tutor.js";
 
 export const findTutors = async (filters) => {
-  const { subject, department, keyword } = filters;
+  const { subject, department, keyword, page = 1, limit = 10 } = filters;
   const query = {};
 
   if (subject) query.subject = { $regex: subject, $options: "i" };
   if (department) query.department = { $regex: department, $options: "i" };
   if (keyword) query.name = { $regex: keyword, $options: "i" };
 
-  return Tutor.find(query);
+  return Tutor.find(query)
+    .skip((page - 1) * limit)
+    .limit(Number(limit));
 };
