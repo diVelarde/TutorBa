b368928f7789b33d6b2e014d6aec435fea271719
diVelarde <divelarde@gbox.adnu.edu.ph>
2025-11-20 03:54:27 +0800

    Create File validators for file

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import multer from "multer";
 import { uploadFileController, getFilesController } from "../controllers/uploadController.js";
-import { validateFile } from "../middleware/validateFileUpload.js"; // optional
+import { validateFileUpload } from "../validators/fileValidator.js";
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.post("/upload", upload.single("file"), validateFile, uploadFileController);
+router.post("/upload", upload.single("file"), validateFileUpload, uploadFileController);
 router.get("/", getFilesController);
 
 export default router;
