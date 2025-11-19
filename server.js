eb58d227f214b68e42e46d2bc07f2a85fa4491cc
diVelarde <divelarde@gbox.adnu.edu.ph>
2025-11-20 03:48:18 +0800

    Added endpoints for uploading and retrieving files

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+import uploadRoutes from "./routes/uploadRoutes.js";
+
 app.use(express.json());
+
+app.use("/api/files", uploadRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
