d01c4e5e95fb707609c518df916564171cf66eeb
IEsmabe <iesmabe@gbox.adnu.edu.ph>
2025-12-12 16:20:38 +0800

    Resolve frontend merge conflicts; remove node_modules from index; finalize frontend fixes

--- models/Favorite.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const FavoriteSchema = new mongoose.Schema({
-  studentId: { type: mongoose.Schema.Types.ObjectId, ref: "StudentProfile", required: true },
-  tutorId: { type: mongoose.Schema.Types.ObjectId, ref: "Tutor", required: true },
-  createdAt: { type: Date, default: Date.now },
-});
-
-export default mongoose.model("Favorite", FavoriteSchema);
